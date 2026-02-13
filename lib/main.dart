31cf9e8cbe3f7df63be94551b0c8d3f901112a86
yaseumine <yasminmaharani123@gmail.com>
2026-02-13 15:11:29 +0700

    membuat wrap expanded dan coloumn

--- lib/main.dart
+++ lib/main.dart
@@ -30,44 +30,51 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Positioned(
-            top: 50,
-            left: 50,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ValueListenableBuilder(
-                valueListenable: counter,
-                builder: (context, score, child) {
-                  return Text(
-                    'Score: $score',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          Positioned(
-            top: 50,
-            right: 50,
-            child: Row(
+          Expanded(
+            child: Stack(
               children: [
-                IconButton(
-                  icon: const Icon(Icons.music_note, color: Colors.black),
-                  onPressed: () {},
+                Positioned(
+                  top: 50,
+                  left: 50,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ValueListenableBuilder(
+                      valueListenable: counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.volume_up, color: Colors.black),
-                  onPressed: () {},
+                Positioned(
+                  top: 50,
+                  right: 50,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.music_note, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.volume_up, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
